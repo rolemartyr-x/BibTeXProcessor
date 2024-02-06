18a0f70fcad20a3e0647020747104ae834b5701b
Jake Tauer <129777633+JakeTauerGA@users.noreply.github.com>
2024-02-06 13:35:28 -0600

    CGPT's first attempt

--- main.ts
+++ main.ts
@@ -1,134 +1,114 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	//App,
+	//CachedMetadata,
+	//Editor,
+	//MarkdownView,
+	//Modal,
+	//Notice,
+	Plugin,
+	//PluginSettingTab,
+	//Setting
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Reference {
+    title: string;
+    publisher: string;
+    volume: string;
+    author: string;
+    year: number;
+    pages: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface Author {
+    name: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+interface BibTeXData {
+    references: Reference[];
+    authors: Author[];
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+export default class BibTeXProcessorPlugin extends Plugin {
+    async onload() {
+        // Register command to process BibTeX input
+        this.addCommand({
+            id: 'process-bibtex',
+            name: 'Process BibTeX',
+            callback: () => this.processBibTeX(),
+        });
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    async processBibTeX() {
+        // Parse BibTeX input (Assuming you have a function to parse BibTeX)
+        const bibtexData = this.parseBibTeX(); // Replace with actual parsing logic
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+        // Generate folder hierarchy
+        const sourcesFolder = await this.app.vault.createFolder('Sources');
+        const authorsFolder = await sourcesFolder.createFolder('Authors');
+        const referencesFolder = await sourcesFolder.createFolder('References');
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        // Process references
+        for (const reference of bibtexData.references) {
+            // Extract reference information
+            const { title, publisher, volume, author, year, pages } = reference;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+            // Create reference page
+            const referencePagePath = `References/${title}.md`;
+            const referencePage = await this.app.vault.createMarkdownFile(referencePagePath);
+            const referenceContent = `# ${title}`;
+            await referencePage.write(referenceContent);
 
-	display(): void {
-		const {containerEl} = this;
+            // Add YAML frontmatter
+            await referencePage.changeFrontmatter((data: FrontMatterData) => {
+                data.title = title;
+                data.publisher = publisher;
+                data.volume = volume;
+                data.author = author;
+                data.year = year;
+                data.pages = pages;
+            });
+        }
 
-		containerEl.empty();
+        // Process authors
+        for (const author of bibtexData.authors) {
+            // Extract author information
+            const { name } = author;
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+            // Create author page
+            const authorPagePath = `Authors/${name}.md`;
+            const authorPage = await this.app.vault.createMarkdownFile(authorPagePath);
+            const authorContent = `# ${name}`;
+            await authorPage.write(authorContent);
+
+            // Add YAML frontmatter
+            await authorPage.changeFrontmatter((data: FrontMatterData) => {
+                data.name = name;
+            });
+        }
+
+        // Display success message
+        this.app.workspace.nag("BibTeX processing complete!");
+    }
+
+    parseBibTeX(): BibTeXData {
+        // Replace this function with actual BibTeX parsing logic
+        // This function should return an object with references and authors arrays
+        return {
+            references: [
+                {
+                    title: "Sample Reference",
+                    publisher: "Sample Publisher",
+                    volume: "1",
+                    author: "John Doe",
+                    year: 2023,
+                    pages: 100
+                }
+            ],
+            authors: [
+                { name: "John Doe" },
+                { name: "Jane Smith" }
+            ]
+        };
+    }
 }
